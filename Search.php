ecbfff794018b94184dd5dd33e74708f13c9ef92
16Diallom <230089347@aston.ac.uk>
2024-11-25 22:39:04 +0000

    Create Search.php

---
+++ Search.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "cs2team48";
+$password = "9ZReO56gOBkKTcr";
+$dbname = "cs2team48_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
