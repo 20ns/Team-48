ad82a712db87d9de996f59860fead34065585234
20Ns <230090446@aston.ac.uk>
2024-12-04 23:30:50 +0000

    added css for the search bar, js for the search bar

---
+++ assets/js/search-bar.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('./search-bar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('search-bar-container').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading search bar:', error));
+  });
+  
+
+// Search functionality
+function searchMenu() {
+  const searchTerm = document.getElementById('menuSearch').value.toLowerCase();
+  const menuItems = document.querySelectorAll('.main-item');
+  
+  menuItems.forEach(item => {
+    const itemName = item.querySelector('.card-title').textContent.toLowerCase();
+    if (itemName.includes(searchTerm)) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
