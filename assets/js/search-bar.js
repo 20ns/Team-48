7580f334cb4f876954608a3af0a057f4a3bb708a
20Ns <230090446@aston.ac.uk>
2024-12-04 23:50:03 +0000

    Added search funtionality to all categories of food

--- assets/js/search-bar.js
+++ assets/js/search-bar.js
@@ -1,18 +1,22 @@
+// Ensure the DOM is fully loaded before executing the script
 window.addEventListener('DOMContentLoaded', () => {
-    fetch('./search-bar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('search-bar-container').innerHTML = data;
-      })
-      .catch(error => console.error('Error loading search bar:', error));
-  });
-  
+  fetch('./search-bar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('search-bar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading search bar:', error));
+
+  // Attach search functionality
+  const searchInput = document.getElementById('searchInput');
+  searchInput.addEventListener('input', searchMenu);
+});
 
 // Search functionality
 function searchMenu() {
-  const searchTerm = document.getElementById('menuSearch').value.toLowerCase();
+  const searchTerm = document.getElementById('searchInput').value.toLowerCase();
   const menuItems = document.querySelectorAll('.main-item');
-  
+
   menuItems.forEach(item => {
     const itemName = item.querySelector('.card-title').textContent.toLowerCase();
     if (itemName.includes(searchTerm)) {
