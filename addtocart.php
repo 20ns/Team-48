b6d7ab085c3645b4907aced06b65552672062411
OliverLacey <230087893@aston.ac.uk>
2024-12-06 19:06:38 +0000

    Adds to database now

--- addtocart.php
+++ addtocart.php
@@ -8,18 +8,24 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$id = $_POST['id'];
-$name = $_POST['name'];
-$price = $_POST['price'];
-$quantity = $_POST['quantity'];
-$sql = "INSERT INTO items (id, name, price, quantity)
-VALUES ($id, $name, $price, $quantity)";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+if(isset($_POST["addtocart"])) {
+  $id = $_POST["id"];
+  $name = $_POST["name"];
+  $price = $_POST["price"];
+  $quantity = $_POST["quantity"];
+
+  $stmt = mysqli_prepare($conn, "INSERT INTO items(id, name, quantity, price) VALUES (?, ?, ?, ?)");
+  mysqli_stmt_bind_param($stmt, 'isdi', $id, $name, $quantity, $price);
+
+
+  if (mysqli_stmt_execute($stmt)) {
+    echo "Item added to cart successfully.";
+    
+    header("Location: basket.html");
+    exit();
+  } else {
+    echo "Error: " . mysqli_error($conn);
+  }
 }
-
-$conn->close();
-?> 
+?>
